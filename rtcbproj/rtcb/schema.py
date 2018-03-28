9df5317df6067a58611d343e4b64e3a27b3e319b
Filippo Campi <filippo.campi@redturtle.it>
2018-03-28 22:33:08 +0200

    [dev] added schema for all model + added urls for for graphiql view + updates settings

--- rtcbproj/rtcb/schema.py
+++ rtcbproj/rtcb/schema.py
@@ -1,13 +1,42 @@
 # -*- coding: utf-8 -*-
-from graphene_django import DjangoConnectionField, filter
+from graphene_django import DjangoConnectionField
+from graphene_django import DjangoObjectType
+from .models import Match as match_model
+from .models import Player as player_model
+from .models import Team as team_model
 
 import graphene
+
+
+class Player(DjangoObjectType):
+    class Meta:
+        model = player_model
+        interfaces = (graphene.Node, )
+
+
+class Team(DjangoObjectType):
+    class Meta:
+        model = team_model
+        interfaces = (graphene.Node, )
+
+
+class Match(DjangoObjectType):
+    class Meta:
+        model = match_model
+        interfaces = (graphene.Node, )
 
 
 class Query(graphene.ObjectType):
 
     # NODO GENERICO
     node = graphene.Node.Field()
+
+    # Player
+    players = DjangoConnectionField(
+        Player,
+        description="all players"
+    )
+    player = graphene.Node.Field(Player)
 
 
 class Mutation(graphene.ObjectType):
@@ -18,5 +47,4 @@ class Mutation(graphene.ObjectType):
 
 schema = graphene.Schema(
     query=Query,
-    mutation=Mutation
 )
--- rtcbproj/rtcb/urls.py
+++ rtcbproj/rtcb/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import PrivateGraphQLView
-from django.views.decorators.csrf import csrf_exempt
 from . import views
 from django.conf.urls import url
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    url(r'^graphql', csrf_exempt(PrivateGraphQLView.as_view(graphiql=True))),
+    url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('', views.index, name='index'),
 ]
