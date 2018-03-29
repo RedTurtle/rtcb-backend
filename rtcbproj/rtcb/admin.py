f6753dea5e8bd81a60c593f52799b9099b522064
Giacomo Monari <arsenico13@hotmail.it>
2018-03-29 22:58:55 +0200

    [new][chg] update populate script with the new models

--- rtcbproj/rtcb/admin.py
+++ rtcbproj/rtcb/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from rtcb.tournament.models import Match
+from rtcb.tournament.models import Match, Tournament
 from rtcb.team.models import Team
 from .authentication.models import User
 
 
 admin.site.register(Team)
+admin.site.register(Tournament)
 admin.site.register(Match)
 admin.site.register(User)
