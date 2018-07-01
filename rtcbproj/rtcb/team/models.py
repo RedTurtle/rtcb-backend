68273dac0349acca26741053c63d80f1812176cf
Giacomo Monari <arsenico13@hotmail.it>
2018-07-02 00:39:18 +0200

    [new] added tournament FK to the Team model

--- rtcbproj/rtcb/team/models.py
+++ rtcbproj/rtcb/team/models.py
@@ -20,5 +20,12 @@ class Team(models.Model):
         related_name="striker_player",
     )
 
+    tournament = models.ForeignKey(
+        'Tournament',
+        null=True,
+        on_delete=models.SET_NULL,
+        # related_name="toru",
+    )
+
     def __str__(self):
         return "Team: {}".format(self.name)
