31a993df82f7c70ccc536479a2c73512de11ecdc
Giacomo Monari <arsenico13@hotmail.it>
2018-07-05 20:17:49 +0200

    [chg] WIP updateTournament placeholder

--- rtcbproj/rtcb/tournament/service.py
+++ rtcbproj/rtcb/tournament/service.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .models import Match as match_model
 from .models import Tournament as tournament_model
+from rtcb.utils import extract_value_from_input
 
 
 class MatchService(object):
@@ -13,10 +14,17 @@ class MatchService(object):
     def createTournament(self, input):
         """ Crea un nuovo torneo.
         """
-
+        # TOTEST
         name = input.get('name')
 
         new_tour = tournament_model(name=name)
 
         new_tour.save()
         return new_tour
+
+    def updateTournament(self, input):
+        """ Modifica dei dati di un torneo.
+        """
+
+        # TOTEST
+        pass
