99a880bf126af87ff407873dc807efbf9ce0f791
Filippo Campi <filippo.campi@redturtle.it>
2018-11-15 22:20:06 +0100

    [dev] authentication

--- rtcbproj/rtcb/views.py
+++ rtcbproj/rtcb/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 from rtcb.team.models import Team
--- rtcbproj/rtcbproj/config/settings.py
+++ rtcbproj/rtcbproj/config/settings.py
@@ -3,6 +3,8 @@
 from .base import *  # noqa
 from .omissis import *  # noqa
 
+import datetime
+
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
@@ -17,9 +19,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rtcb',
 
+    'rest_framework',
+    'rest_framework_jwt',
     'graphene_django',
+    'rtcb',
 ]
 
 GRAPHENE = {
@@ -37,6 +41,20 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'rtcbproj.urls'
+
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
 
 
 # sqlite3 database - dev
